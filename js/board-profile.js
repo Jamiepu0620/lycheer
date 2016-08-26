a3ac43b28278110b5aab640b1de6e2f092040d8c
xuehuiilu <1502324900@qq.com>
2016-08-26 18:57:52 +0800

    修改了课程和讲师直播间

---
+++ js/board-profile.js
@@ -0,0 +1,13 @@
+$(function(){
+
+	// 我的二维码弹窗
+	$("#show_myqrcode").click(function(){
+		$("#my_qrcode_box").show();
+	});
+
+	// 分享弹窗
+	$("#btn_studio_share").click(function(){
+		$("#share_cover_box").show();
+	})
+
+});
